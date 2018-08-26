09533f6e5f949c73231731640dc6e4f2e9cc4828
Elias Meyer <elias.meyer@operacao.rcadigital.com.br>
2018-08-26 20:21:59 -0300

    Setting heroku HOST_PORT.

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -12,29 +12,29 @@ import java.io.File;
 import java.io.IOException;
 
 public final class Main {
-  
-  private static final int HTTP_PORT = 8080;
-  private static final String CONTEXT_APP = "/transpoBrasil";
-  private static final String WEB_DIR_LOCATION = "src/main/webapp/";
-  
-  private Main() {}
-  
-  public static void main(String[] args) throws ServletException, LifecycleException, IOException {
-    
-    //Tomcat instance
-    Tomcat tomcat = new Tomcat();
-    tomcat.setPort(HTTP_PORT);
-    
-    StandardContext ctx = (StandardContext) tomcat.addWebapp(CONTEXT_APP, new File(WEB_DIR_LOCATION).getCanonicalPath());
-    System.out.println("configuring app with basedir: " + new File( WEB_DIR_LOCATION).getCanonicalPath());
-    
-    File additionWebInfClasses = new File("target/classes");
-    WebResourceRoot resources = new StandardRoot(ctx);
-    resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
-        additionWebInfClasses.getAbsolutePath(), "/"));
-    ctx.setResources(resources);
-    
-    tomcat.start();
-    tomcat.getServer().await();
-  }
+    private static final String PORT = "PORT";
+    private static final String CONTEXT_APP = "/transpoBrasil";
+    private static final String WEB_DIR_LOCATION = "src/main/webapp/";
+
+    private Main() {
+    }
+
+    public static void main(String[] args) throws ServletException, LifecycleException, IOException {
+
+        //Tomcat instance
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(Integer.parseInt(System.getenv(PORT)));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp(CONTEXT_APP, new File(WEB_DIR_LOCATION).getCanonicalPath());
+        System.out.println("configuring app with basedir: " + new File(WEB_DIR_LOCATION).getCanonicalPath());
+
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
 }
